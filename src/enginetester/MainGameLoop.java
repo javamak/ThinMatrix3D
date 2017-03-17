737c7e654bdffb3896d4d6adac7781e04adada82
javamak <javamak@gmail.com>
2017-03-17 21:18:00 +0530

    Display Manager added and test class added

---
+++ src/enginetester/MainGameLoop.java
@@ -0,0 +1,17 @@
+package enginetester;
+
+import org.lwjgl.opengl.Display;
+
+import renderengine.DisplayManager;
+
+public class MainGameLoop {
+
+	public static void main(String[] args) {
+		DisplayManager.createDisplay();
+		
+		while (!Display.isCloseRequested()) {
+			DisplayManager.updateDisplay();
+		}
+		DisplayManager.closeDisplay();
+	}
+}
