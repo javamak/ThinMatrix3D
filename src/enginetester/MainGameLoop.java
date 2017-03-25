75ce99e78e24117422e9e4949abb5a4640144daf
javamak <javamak@gmail.com>
2017-03-25 16:44:34 +0530

    Tut11

--- src/enginetester/MainGameLoop.java
+++ src/enginetester/MainGameLoop.java
@@ -5,6 +5,7 @@ import org.lwjgl.util.vector.Vector3f;
 
 import entities.Camera;
 import entities.Entity;
+import entities.Light;
 import models.RawModel;
 import models.TexturedModel;
 import renderengine.DisplayManager;
@@ -18,24 +19,27 @@ public class MainGameLoop {
 
 	public static void main(String[] args) {
 		DisplayManager.createDisplay();
-		
+
 		Loader loader = new Loader();
 		StaticShader shader = new StaticShader();
 		Renderer renderer = new Renderer(shader);
-		
+
 		RawModel model = OBJLoader.loadObjModel("stall", loader);
 		ModelTexture texture = new ModelTexture(loader.loadTexture("stallTexture"));
 		TexturedModel texturedModel = new TexturedModel(model, texture);
-		Entity entity = new Entity(texturedModel, new Vector3f(0, 0, -10), 0, 0, 0, 1);
-		
+		Entity entity = new Entity(texturedModel, new Vector3f(0, -2, -25), 0, 0, 0, 1);
+
+		Light light = new Light(new Vector3f(0, 0, 20), new Vector3f(1, 1, 1));
+
 		Camera camera = new Camera();
-		
+
 		while (!Display.isCloseRequested()) {
 			entity.increaseRotation(0, 1, 0);
 			camera.move();
 			renderer.prepare();
-			//Game logic
+			// Game logic
 			shader.start();
+			shader.loadLight(light);
 			shader.loadViewMatrix(camera);
 			renderer.render(entity, shader);
 			shader.stop();
